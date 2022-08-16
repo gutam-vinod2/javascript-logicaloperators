918f0f1e205cc8e695a6146071eef37de6a94811
[firstname lastname] <[valid-email]>
2022-08-16 19:26:01 +0530

    logical

---
+++ logical operators/logicaloperators.js
@@ -0,0 +1,36 @@
+var a = 5;
+var b = 10;
+var x = (a != b) && (a < b);
+console.log(x)
+
+
+
+var a = 5;
+var b = 10;
+var x = (a > b) || (a == b);
+console.log(x)
+
+
+var a = 5;
+var b = 10;
+var x = (a < b) || (a == b);
+console.log(x)
+
+
+
+var a = 5;
+var b = 10;
+var x = ! (a <b);
+console.log(x)
+
+
+
+
+var a = 5;
+var b = 10;
+var x = ! (a >b);
+console.log(x)
+
+
+
+
